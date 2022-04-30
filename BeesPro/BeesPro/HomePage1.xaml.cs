7d64bb032b26d24637032e5726157f7330056dd2
FireC1993 <ologbonilemobayo2000@gmail.com>
2022-04-30 16:16:04 +0100

    Add project files.

---
+++ BeesPro/BeesPro/HomePage1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BeesPro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage1 : TabbedPage
+    {
+        public HomePage1()
+        {
+            InitializeComponent();
+        }
+        
+        private void ToolbarItem_Cliked(object sender, EventArgs e) 
+        {
+            Navigation.PushAsync(new NavigationPage());
+        }
+    }
+}
